4975a2da645fb2636bb8cac154778dff9c333df7
Lon Heligas <lon.heligas@gmail.com>
2023-04-11 12:36:41 -0500

    initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as IntroPage} from './IntroPage'
+export { default as PortfolioPage } from './PortfolioPage'
+export { default as ContactPage } from './ContactPage'
+export { default as ResumePage } from './ResumePage'
---
+++ src/portfolio-data/ProfileCreatorAppData.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import profileImage from "../images/profilecreatorimg.png"
+import quizforgeeks from "../images/quizforgeeks.png"
+import randompasswordgenerator from "../images/randompasswordgenerator.png"
+import smorgasbord from "../images/smorgasbord.png"
+import weattheruneed2know from "../images/weattheruneed2know.png"
+import findagame from "../images/findagame.png"
+
+const ProfileCreatorApp = [
+  {title:  "Find a Free Game",
+  text: "Find a free game in our easy to use search engine.",
+  gitUrl: "https://github.com/lonHeligas/find-videogame-project-main-local",
+  webUrl: "",
+  img: findagame},
+
+  {title:  "Smorgasbord",
+  text: "Find a new restaurant to visit with our simple user-generated ratings board.",
+  gitUrl: "https://github.com/lonHeligas/smorgasbord-local",
+  webUrl: "https://smorgasbord.herokuapp.com/",
+  img: smorgasbord}, 
+
+  {title:  "Profile Creator for Your Team",
+  text: "Create your team members with specific information for each role. ",
+  gitUrl: "https://github.com/lonHeligas/profile-creator-for-your-team",
+  webUrl: "",
+  img: profileImage}, 
+  
+  {title:  "Password Generator",
+  text: "Create a randomly generated password with paramaters you define",
+  gitUrl: "https://github.com/lonHeligas/random-password-generator",
+  webUrl: "https://lonheligas.github.io/random-password-generator/",
+  img: randompasswordgenerator},
+
+  {title:  "Weather U Need to Know",
+  text: "Find out the weather for your location. You need to know it.",
+  gitUrl: "https://github.com/lonHeligas/weather-u-need-to-know",
+  webUrl: "https://lonheligas.github.io/weather-u-need-to-know/",
+  img: weattheruneed2know},
+  
+  {title:  "Quiz for Geeks and Other People",
+  text: "A quick quiz for geeks and other people",
+  gitUrl: "https://github.com/lonHeligas/quiz-for-geeks-and-other-people",
+  webUrl: "https://lonheligas.github.io/quiz-for-geeks-and-other-people/",
+  img: quizforgeeks}]
+
+
+export default ProfileCreatorApp
+
+
+
+
+
+
+
+
+
+
+
+
